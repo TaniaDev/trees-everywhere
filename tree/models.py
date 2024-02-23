91eb03a0e1cedc18da51b48ad202c026b7e737a0
Tânia de Arruda <tandearruda@gmail.com>
2024-02-23 07:37:27 -0300

    feat: update PlantedTree with coordinate location and automatic age

--- tree/models.py
+++ tree/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.db import models
+from django.db.models import ExpressionWrapper, F, Func
 
 
 class Tree(models.Model):
@@ -9,12 +12,23 @@ class Tree(models.Model):
         return str(self.name)
 
 
+class YearsDiff(Func):
+    function = "EXTRACT"
+    template = "%(function)s(YEAR FROM %(expressions)s)"
+    output_field = models.IntegerField()
+
+
 class PlantedTree(models.Model):
-    age = models.IntegerField()
     planted_at = models.DateTimeField()
+    age = ExpressionWrapper(YearsDiff(datetime.now(), F("planted_at")), output_field=models.IntegerField())
     user = models.ForeignKey("user.User", on_delete=models.CASCADE, null=True)
     account = models.ForeignKey("account.Account", on_delete=models.CASCADE, null=True)
     tree = models.ForeignKey("tree.Tree", on_delete=models.CASCADE, null=True)
+    latitude_location = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    longitude_location = models.DecimalField(max_digits=9, decimal_places=6, null=True)
 
     def __str__(self):
         return str(self.tree)
+
+    def get_planted_tree_location(self, planted_tree):
+        return (planted_tree.latitude_location, planted_tree.longitude_location)
