48664561775c277fbfa83ab1f23be9a7178bc060
Tânia de Arruda <tandearruda@gmail.com>
2024-02-23 02:09:26 -0300

    feat: allow activate and deactivate accounts on admin list accounts

--- account/admin.py
+++ account/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from account.models import Account
 
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "active")
+    actions = ["activate_accounts", "deactivate_accounts"]
+
+    @admin.action(description="Activate selected accounts")
+    def activate_accounts(self, request, queryset):
+        queryset.update(active=True)
+
+    @admin.action(description="Deactivate selected accounts")
+    def deactivate_accounts(self, request, queryset):
+        queryset.update(active=False)
+
+
 # Register your models here.
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
