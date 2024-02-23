69bcdad703f101a1f31d4ef4827e54981d087250
Tânia de Arruda <tandearruda@gmail.com>
2024-02-23 07:33:25 -0300

    feat: add authentication validation and templates

--- project/settings.py
+++ project/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -108,6 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "user.User"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.cache import never_cache
 
-# Create your views here.
+
+@never_cache
+def user_login(request):
+    if request.method != "POST":
+        return redirect("dashboard") if request.user.is_authenticated else render(request, "login.html")
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    if not (user := authenticate(username=username, password=password)):
+        return render(request, "login.html", {"error": "Authentication failed: email or password is incorrect."})
+    login(request, user)
+    return redirect("/")
+
+
+@login_required
+def dashboard(request):
+    if request.user.is_authenticated:
+        if request.method == "POST" and "logout" in request.POST:
+            logout(request)
+            return redirect("/")
+        return render(request, "index.html")
